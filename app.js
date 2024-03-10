fc60b6e885bc61706f26691052cc64c2979edee1
Sanzai-X <130345226+Sanzai-X@users.noreply.github.com>
2024-03-10 21:30:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const expbs = require('express-handlebars');
-const UserModel = require('./models/user.js');
-const restoModel = require('./models/restaurant.js');
+const UserModel = require('./models/user');
+const restoModel = require('./models/restaurant');
+
+const DB_URL = "mongodb+srv://dbAdmin:dALTIIUAe1845fGW@restaurant-review.u6ovtf3.mongodb.net/ReviewWebsite?retryWrites=true&w=majority&appName=restaurant-review";
 
 mongoose.connect("mongodb://localhost:27017/ReviewWebsite");
+
+app.use(bodyparser.urlencoded(
+    {
+        extended: false
+    }
+));
 
 app.engine('handlebars', expbs.engine({
 	defaultLayout: "main",
@@ -30,4 +38,28 @@ app.get('/', async (req,res) => {
 	let curruser = await UserModel.findById("65e833ffc7b2c99b7605f6ee");
 	res.render('index', {title: "Home page", h_user: curruser.getUsername, img_url: curruser.getImage, restaurants: trending});
 	console.log(trending);
+});
+
+app.get('/login', (req, res) => {
+    res.render('login',{ title: "Login Account"});
+});
+
+// this part will check if the given login credetnials is valid assuming that it has the 
+// exact record in the database, if so, it will then proceed
+// the login process and go to the main page
+app.post("/login", bodyparser.json(), async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username, password: req.body.password});
+        console.log(user);
+        if (user) {  
+            console.log('logged in successfully');
+            res.redirect("/index"); // this part will redirect the user to the main page
+        } else {
+            console.log('did not login successfully');
+            res.render("login", { title: "Login Account", error: "Invalid username or password" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+    }
 });
