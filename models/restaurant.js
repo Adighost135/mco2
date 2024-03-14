adf155bb1b00848f01f55121faabfc5cba7773e6
ReythemGespenst <151455874+ReythemGespenst@users.noreply.github.com>
2024-03-15 01:09:55 +0800

    Add files via upload

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,35 +1,49 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const databaseURL = "mongodb://localhost:27017/ReviewWebsite"
+const options = {useNewUrlParser: true};
 
-const restaurantSchema = new Schema({
-    name: {
-        type: String, 
-        required: true
-    },
-    images: {
-        type: [String],
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: mongoose.SchemaTypes.ObjectId,
-        required: true
-    },
-    rating: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    reviews: {
-        type: [String]
-    }
-}, { timestamps: true });
+mongoose.connect(databaseURL, options);
 
-const Restaurant = mongoose.model("Restaurant", restaurantSchema);
-module.exports = Restaurant;
+const restaurantSchema = new mongoose.Schema({
+	name: {
+		type: String, 
+		required: true
+	},
+	images: {
+		type: [String],
+		required: true
+	},
+	location: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	owner: {
+		type: mongoose.SchemaTypes.ObjectId,
+		required: true,
+		ref: "Users"
+	},
+	reviews: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Review'
+	}],
+	rating: {
+		type: Number,
+		required: true
+	}
+}, {timestamps: true});
+
+const restoModel = mongoose.model('restaurants', restaurantSchema, "restaurants");
+
+exports.getTop3 = async function(next){
+	try{
+		const result = restoModel.find({}).sort({rating: -1}).limit(3).exec();
+		return result;
+	}
+	catch (err){
+		console.error(err);
+	}
+};
