b8976fb0acc967931c2f21ecb61ec08066ab603a
ReythemGespenst <151455874+ReythemGespenst@users.noreply.github.com>
2024-03-15 22:00:31 +0800

    Add files via upload

---
+++ routes/restaurantRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const restaurantController = require('../controllers/restaurantController');
+const bodyparser = require('body-parser');
+
+router.get('/', restaurantController.getTop3);
+
+router.get('/logged/:id', restaurantController.getTop3);
+
+router.get('/login', restaurantController.loginpage);
+
+router.post('/login', bodyparser.json(), restaurantController.login);
+
+router.get('/viewProfile/logged/:id/profile/:id2', restaurantController.viewProfile);
+
+router.get('/viewRestaurant/:id', restaurantController.viewResto)
+
+module.exports = router;
